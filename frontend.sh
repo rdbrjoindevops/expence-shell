af8e880fc54f660ec930a874d6b58ff8fb8d740d
ravindra <rdbrjoindevops@gmail.com>
2025-01-04 22:01:55 +0530

    expence-project

--- frontend.sh
+++ frontend.sh
@@ -55,7 +55,7 @@ VALIDATE $? "Moving to HTML directory"
 unzip /tmp/frontend.zip &>>$LOG_FILE_NAME
 VALIDATE $? "unzipping the frontend code"
 
-cp /home/ec2-user/expence-shell/expense.conf /etc/nginx/default.d/expense.conf
+cp /home/ec2-user/expence-shell/expence.conf /etc/nginx/default.d/expence.conf
 VALIDATE $? "Copied expense config"
 
 systemctl restart nginx &>>$LOG_FILE_NAME
